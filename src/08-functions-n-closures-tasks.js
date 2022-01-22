b234a9d6706a1bbe8665b35d38d7ba05b29c5dfb
ixakina <ixakina@mail.ru>
2022-01-23 00:49:10 +0300

    feat: add closure task

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -81,8 +81,15 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache = null;
+  return function b() {
+    if (!cache) {
+      cache = func();
+      return cache;
+    }
+    return cache;
+  };
 }
 
 
